135f0d57244a1f9149fb0a8c49d2788148d7fbd2
alissastaylor <alissataylor@Alissas-MacBook-Pro.local>
2017-10-02 23:13:37 -0400

    remove: react; add: reset wp theme

--- src/universal/routes/static.js
+++
@@ -1,4 +0,0 @@
-export { default as Home } from 'universal/views/Home/HomeContainer.jsx';
-export { default as StyleGuide } from 'universal/views/StyleGuide/StyleGuideContainer.jsx';
-export { default as NotFound } from 'universal/modules/Global/NotFound.jsx';
-
---
+++ tpl/tpl-home.php
@@ -0,0 +1,6 @@
+<?php /* Template Name: Homepage */
+get_header(); ?>
+
+  <h1>Alissa</h1>
+
+<?php get_footer(); ?>
