2bd6240e6351c1905d95aec27eb7000926c512f6
alissastaylor <alissataylor@Alissas-MacBook-Pro.local>
2017-10-11 00:00:35 -0400

    add: working css and dummy styles for homepage

--- gulpfile.js
+++ gulpfile.js
@@ -2,52 +2,56 @@ var gulp = require('gulp');
 var clean = require('gulp-clean');
 var jshint = require('gulp-jshint');
 var gutil = require('gulp-util');
-var sass = require('gulp-sass');
+var sass = require('gulp-ruby-sass');
 var minifyCSS = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var imagemin = require('gulp-imagemin');
+var livereload = require('gulp-livereload');
 var uglify = require('gulp-uglify');
 var imageResize = require('gulp-image-resize');
-var compass = require('gulp-compass');
 var cache = require('gulp-cache');
-var webserver = require('gulp-webserver');
 var concat = require('gulp-concat');
-var browserSync = require('browser-sync').create();
+var autoprefixer = require('gulp-autoprefixer');
+var tasklist = require('gulp-task-list')(gulp);
 
-
+var wordpressRoot = ''; // you will need to set up a root directory for your wordpress installation and pass it as an argument to the serve task
+// change these values to point to the custom theme
 var bases = {
-  dev: 'dev/wp/wp-content/themes/talking-rock/',
-  prod: 'prod/wp/wp-content/themes/talking-rock/',
+  dev: 'themes-dev/talking-rock/',
+  prod: 'themes-prod/talking-rock/',
 };
 var paths = {
   images: 'images/',
   js: 'js/',
   css: 'css/'
-}
+};
 
 gulp.task('sass', function () {
-  gulp.src('./scss/*.scss')
-    .pipe(compass({
-      css: 'css',
-      sass: 'scss',
-      image: 'images',
-      javascript: bases.dev + 'js',
-      relative: true
+  return sass('./scss/*.scss')
+    .on('error', sass.logError)
+    .pipe(autoprefixer({
+      cascade: false
     }))
     .pipe(minifyCSS())
-    .pipe(gulp.dest(bases.dev + 'css'))
-    .pipe(browserSync.reload({ stream: true }));
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest(bases.dev + paths.css));
 });
 
+// wipe out the production directory
 gulp.task('clean-prod', function () {
   return gulp.src(bases.prod)
     .pipe(clean());
 });
 
-gulp.task('scripts-prod', ['copy-prod'], function () {
+// prepare js files for production
+gulp.task('scripts-prod', function () {
   gulp.src(paths.js + 'script.js', {
       cwd: bases.dev
     })
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
     .pipe(uglify())
     .pipe(concat('script.min.js'))
     .pipe(gulp.dest(bases.prod + paths.js));
@@ -56,13 +60,9 @@ gulp.task('scripts-prod', ['copy-prod'], function () {
     })
     .pipe(concat('vendor.min.js'))
     .pipe(gulp.dest(bases.prod + paths.js));
-  gulp.src(paths.js + 'acq-scripts/*.js', {
-      cwd: bases.dev
-    })
-    .pipe(concat('acq-scripts.min.js'))
-    .pipe(gulp.dest(bases.prod + paths.js));
 });
 
+// imagemin for production
 gulp.task('imagemin-prod', function () {
   gulp.src(paths.images + '/**/*.*', {
       cwd: bases.dev
@@ -71,7 +71,8 @@ gulp.task('imagemin-prod', function () {
     .pipe(gulp.dest(bases.prod + paths.images));
 });
 
-gulp.task('copy-prod', ['clean-prod'], function () {
+// copies themes-dev directory to a themes-prod folder
+gulp.task('copy-prod', function () {
   // Copy everything
   gulp.src('**/*.*', {
       cwd: bases.dev
@@ -79,6 +80,12 @@ gulp.task('copy-prod', ['clean-prod'], function () {
     .pipe(gulp.dest(bases.prod));
 });
 
+// watches scss for changes and runs livereload listener
+gulp.task('watch', function () {
+  gulp.watch('./scss/**/*.scss', ['sass']);
+});
+
+// optimize images with imagemin (Photoshop save-for-web should be used first)
 gulp.task('imagemin', function () {
   return gulp.src(bases.dev + 'images/**/*')
     .pipe(cache(imagemin({
@@ -89,4 +96,8 @@ gulp.task('imagemin', function () {
     .pipe(gulp.dest(bases.dev + 'images'));
 });
 
-gulp.task('production', ['scripts-prod']);
+// default task runs watch
+gulp.task('default', ['watch']);
+
+// production packaging task
+gulp.task('production', ['clean-prod', 'copy-prod', 'scripts-prod', 'imagemin-prod']);
---
+++ themes-dev/talking-rock/homepage.php
@@ -0,0 +1,53 @@
+
+<?php
+//Template Name: Homepage
+get_header();
+?>
+<div class='homepage'>
+  <h1>Latest News</h1>
+  <div class="latestPost--1">
+    <div class="featured-image">
+      <img src="http://jellydemos.com/html/muziq/muziq-hardrock/images/demo/news/post1.jpg" alt="">
+      <div class="date">10 Oct</div>
+    </div>
+    <div class="postInfo">
+      <div class="date">
+        Date:
+        <span>Oct 10, 2017</span>
+        <h3>Test post about something that is cool and all rocky.</h3>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi a reiciendis molestias, dolorum veritatis facere placeat libero est doloribus, et, omnis laborum esse amet iste corporis soluta asperiores temporibus eaque!</p>
+        <div class='ctaBtn'><a href="">Read More</a></div>
+      </div>
+    </div>
+  </div>
+  <div class="latestPost--2">
+    <div class="featured-image">
+      <img src="http://jellydemos.com/html/muziq/muziq-hardrock/images/demo/news/post2.jpg" alt="">
+      <div class="date">10 Oct</div>
+    </div>
+    <div class="postInfo">
+      <div class="date">
+        Date:
+        <span>Oct 10, 2017</span>
+        <h3>Test post about something that is cool and all rocky.</h3>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi a reiciendis molestias, dolorum veritatis facere placeat libero est doloribus, et, omnis laborum esse amet iste corporis soluta asperiores temporibus eaque!</p>
+        <div class='ctaBtn'><a href="">Read More</a></div>
+      </div>
+    </div>
+  </div>
+  <div class="latestPost--3">
+    <div class="featured-image">
+      <img src="http://jellydemos.com/html/muziq/muziq-hardrock/images/demo/news/post3.jpg" alt="">
+      <div class="date">10 Oct</div>
+    </div>
+    <div class="postInfo">
+      <div class="date">
+        Date:
+        <span>Oct 10, 2017</span>
+        <h3>Test post about something that is cool and all rocky.</h3>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi a reiciendis molestias, dolorum veritatis facere placeat libero est doloribus, et, omnis laborum esse amet iste corporis soluta asperiores temporibus eaque!</p>
+        <div class='ctaBtn'><a href="">Read More</a></div>
+      </div>
+    </div>
+  </div>
+</div>
