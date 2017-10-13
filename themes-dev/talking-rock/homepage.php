a54026af687a7c0594d7c42997d8b8e20e333919
alissastaylor <alissataylor@Alissas-MacBook-Pro.local>
2017-10-12 22:13:04 -0400

    add: actual content for posts on homepage

--- themes-dev/talking-rock/homepage.php
+++ themes-dev/talking-rock/homepage.php
@@ -3,51 +3,35 @@
 //Template Name: Homepage
 get_header();
 ?>
+	
 <div class='homepage'>
-  <h1>Latest News</h1>
-  <div class="latestPost--1">
-    <div class="featured-image">
-      <img src="http://jellydemos.com/html/muziq/muziq-hardrock/images/demo/news/post1.jpg" alt="">
-      <div class="date">10 Oct</div>
-    </div>
-    <div class="postInfo">
-      <div class="date">
-        Date:
-        <span>Oct 10, 2017</span>
-        <h3>Test post about something that is cool and all rocky.</h3>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi a reiciendis molestias, dolorum veritatis facere placeat libero est doloribus, et, omnis laborum esse amet iste corporis soluta asperiores temporibus eaque!</p>
-        <div class='ctaBtn'><a href="">Read More</a></div>
+  <h1>Latest News</h1>  
+  <?php $postslist = get_posts(array(
+    'posts_per_page' => 3,
+    'order' => 'ASC',
+    'orderby' => 'title'
+  ));
+    if ($postslist) {
+      foreach ($postslist as $post) :
+        setup_postdata($post);
+      ?>
+      <div class="latestPost">
+        <div class="featured-image">
+          <img src="<?php the_post_thumbnail_url('medium'); ?> " alt="">
+          <div class="date"><?php the_date(); ?></div>
+        </div>
+        <div class="postInfo">
+          <div class="date">
+            Date:
+            <span><?php the_date(); ?></span>
+            <h3><?php the_title(); ?></h3>
+            <p><?php the_excerpt(); ?></p>
+            <div class='ctaBtn'><a href="">Read More</a></div>
+          </div>
+        </div>
+        <?php
+        endforeach;
+        wp_reset_postdata();
+      } ?>
       </div>
     </div>
-  </div>
-  <div class="latestPost--2">
-    <div class="featured-image">
-      <img src="http://jellydemos.com/html/muziq/muziq-hardrock/images/demo/news/post2.jpg" alt="">
-      <div class="date">10 Oct</div>
-    </div>
-    <div class="postInfo">
-      <div class="date">
-        Date:
-        <span>Oct 10, 2017</span>
-        <h3>Test post about something that is cool and all rocky.</h3>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi a reiciendis molestias, dolorum veritatis facere placeat libero est doloribus, et, omnis laborum esse amet iste corporis soluta asperiores temporibus eaque!</p>
-        <div class='ctaBtn'><a href="">Read More</a></div>
-      </div>
-    </div>
-  </div>
-  <div class="latestPost--3">
-    <div class="featured-image">
-      <img src="http://jellydemos.com/html/muziq/muziq-hardrock/images/demo/news/post3.jpg" alt="">
-      <div class="date">10 Oct</div>
-    </div>
-    <div class="postInfo">
-      <div class="date">
-        Date:
-        <span>Oct 10, 2017</span>
-        <h3>Test post about something that is cool and all rocky.</h3>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi a reiciendis molestias, dolorum veritatis facere placeat libero est doloribus, et, omnis laborum esse amet iste corporis soluta asperiores temporibus eaque!</p>
-        <div class='ctaBtn'><a href="">Read More</a></div>
-      </div>
-    </div>
-  </div>
-</div>
