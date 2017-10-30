3569849d76590b83946de048a1d365cead253354
Alissa Taylor <ataylor@hirezstudios.com>
2017-10-30 18:18:27 -0400

    add: google analytics to header of theme

--- themes-dev/rock-n-rolla/header.php
+++ themes-dev/rock-n-rolla/header.php
@@ -17,7 +17,15 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-108611471-1"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
 
+    gtag('config', 'UA-108611471-1');
+</script>
 <?php wp_head(); ?>
 </head>
 
